af6fb7d853b241c07a4383429183a7d3f659fcec
AllanLoo <l_aiyong@163.com>
2015-08-06 23:41:18 +0800

    添加注解

--- security/src/main/java/com/isp/security/func/service/FuncServcie.java
+++ security/src/main/java/com/isp/security/func/service/FuncServcie.java
@@ -3,12 +3,14 @@ package com.isp.security.func.service;
 import com.isp.common.service.CrudService;
 import com.isp.security.func.dao.FuncDao;
 import com.isp.security.func.entity.Func;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by AllanLoo on 2015/8/5.
  */
+@Service
 public class FuncServcie extends CrudService<FuncDao,Func>{
     /**
      * 根据用户id获取功能菜单
--- security/src/main/java/com/isp/security/role/service/RoleService.java
+++ security/src/main/java/com/isp/security/role/service/RoleService.java
@@ -3,9 +3,11 @@ package com.isp.security.role.service;
 import com.isp.common.service.CrudService;
 import com.isp.security.role.dao.RoleDao;
 import com.isp.security.role.entity.Role;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by AllanLoo on 2015/8/5.
  */
+@Service
 public class RoleService extends CrudService<RoleDao,Role>{
 }
