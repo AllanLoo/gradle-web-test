d47b9f8323402bf31f9c0a9db6115c5e0e929b28
allanloo <845730148@qq.com>
2015-09-06 17:44:23 +0800

    删除response参数

--- common/src/main/java/com/isp/common/web/bean/Page.java
+++ common/src/main/java/com/isp/common/web/bean/Page.java
@@ -35,30 +35,29 @@ public class Page<T> {
     public Page() {
     }
 
+
     /**
      * 构造方法
-     * @param request 传递 repage 参数，来记住页码
-     * @param response 用于设置 Cookie，记住页码
+     * @param request 传递 page 参数，来记住页码
      */
-    public Page(HttpServletRequest request, HttpServletResponse response){
-        this(request, response, -2);
+    public Page(HttpServletRequest request){
+        this(request, -2);
     }
 
     /**
      * 构造方法
-     * @param request 传递 repage 参数，来记住页码
-     * @param response 用于设置 Cookie，记住页码
+     * @param request 传递 page 参数，来记住页码
      * @param defaultPageSize 默认分页大小，如果传递 -1 则为不分页，返回所有数据
      */
-    public Page(HttpServletRequest request, HttpServletResponse response, int defaultPageSize){
-        // 设置页码参数（传递repage参数，来记住页码）
+    public Page(HttpServletRequest request,int defaultPageSize){
+        // 设置页码参数（传递page参数，来记住页码）
         String no = request.getParameter("pageNo");
         if (StringUtils.isNumeric(no)){
             this.setPageNo(Integer.parseInt(no));
         }
-        // 设置页面大小参数（传递repage参数，来记住页码大小）
+        // 设置页面大小参数（传递page参数，来记住页码大小）
         String size = request.getParameter("pageSize");
-        if (StringUtils.isNumeric(size)){
+        if (StringUtils.isNumeric(size)) {
             this.setPageSize(Integer.parseInt(size));
         }else if (defaultPageSize != -2){
             this.pageSize = defaultPageSize;
